9cdb7b2b7a2960c62d3d4c18c57d5d7022abb8ab
Nurmayanti091 <146069633+Nurmayanti091@users.noreply.github.com>
2023-12-18 13:56:56 +0700

    Delete bilangan.js

--- bilangan.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#jumlahkan').addEventListener('click', function() {
-    let bil1 = parseInt(document.querySelector('#bil1').value);
-    let bil2 = parseInt(document.querySelector('#bil2').value);
-    let hasil = bil1 + bil2;
-    document.querySelector('#hasil').textContent = 'Hasil: ' + hasil;
-});
